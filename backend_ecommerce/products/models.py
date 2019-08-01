de92a28c47cb1cd7cc83b0450434d5066b89023d
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-08-01 15:36:00 +0530

    added imagefeild

--- backend_ecommerce/products/models.py
+++ backend_ecommerce/products/models.py
@@ -1,4 +1,23 @@
+import os
+import random
 from django.db import models
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    # print(instance)
+    #print(filename)
+    new_filename = random.randint(1,3910209312)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_filename}".format(
+            new_filename=new_filename, 
+            final_filename=final_filename
+            )
 
 # Create your models here.
 class Product(models.Model):
@@ -13,6 +32,7 @@ class Product(models.Model):
     sna3            = models.CharField(max_length=30)
     service_type    = models.IntegerField()
     price           = models.DecimalField(decimal_places=2, max_digits=20)
+    image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
         return self.title
