899ff735f45a44a79c4d78239715bd85795bcee6
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-08-02 02:28:00 +0530

    add reverse for urls

--- backend_ecommerce/products/models.py
+++ backend_ecommerce/products/models.py
@@ -1,6 +1,7 @@
 import os
 import random
 from django.db import models
+from django.urls import reverse
 from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
 
@@ -64,7 +65,8 @@ class Product(models.Model):
     objects = ProductManager()
 
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug=self.slug)
+        # return "/products/{slug}/".format(slug=self.slug)
+        return reverse("products:detail", kwargs={"slug": self.slug})
 
     def __str__(self):
         return self.title
