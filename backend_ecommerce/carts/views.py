5e7a157da02bbe2661c3f3570f02ceb5951473aa
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-08-02 23:14:58 +0530

    handle ajax

--- backend_ecommerce/carts/views.py
+++ backend_ecommerce/carts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 from accounts.forms import LoginForm, GuestForm
 from addresses.forms import AddressForm
@@ -17,8 +18,8 @@ def cart_home(request):
 def cart_update(request):
     product_id = request.POST.get('product_id')
 
-    if request.is_ajax():
-        print("Ajax request")
+    # if request.is_ajax():
+    #     print("Ajax request")
 
     if product_id is not None:
         try:
@@ -29,11 +30,21 @@ def cart_update(request):
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
+            added = False
         else:
             cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
-        
+            added = False
         request.session['cart_items'] = cart_obj.products.count()
         # return redirect(product_obj.get_absolute_url())
+        
+        if request.is_ajax(): # Asynchronous JavaScript And XML / JSON
+            print("Ajax request")
+            json_data = {
+                "added": added,
+                "removed": not added,
+            }
+            return JsonResponse(json_data)
+            
     return redirect("cart:home")
 
 def checkout_home(request):
