49b78181a25ca91b02dc35ef5c565fb0e11ef442
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-08-02 10:49:49 +0530

    sessions

--- backend_ecommerce/carts/views.py
+++ backend_ecommerce/carts/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def cart_home(request):
-    return render(request, 'carts/home.html', {})
+    # print(request.session) # on the request
+    # print(dir(request.session))
+    # request.session.set_expiry(300) # 5 minutes
+    # key = request.session.session_key
+    # print(key)
+    request.session['cart_id'] = 12 # Set
+    request.session['user'] = request.user.username
+    return render(request, "carts/home.html", {})
