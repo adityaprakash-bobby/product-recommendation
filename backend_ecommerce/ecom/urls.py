f2315d7212a933fa836fd9e93a48b58a83411e9e
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-08-02 19:20:28 +0530

    reuse reciever address

--- backend_ecommerce/ecom/urls.py
+++ backend_ecommerce/ecom/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 
 from .views import home_page, about_page, contact_page
-from addresses.views import checkout_address_create_view
+from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from accounts.views import login_page, register_page, guest_register_view
 
 urlpatterns = [
@@ -35,7 +35,8 @@ urlpatterns = [
     url(r'^products/', include("products.urls", namespace='products')),
     url(r'^search/', include("search.urls", namespace='search')),
     url(r'^cart/', include( "carts.urls", namespace='cart')),
-    url(r'^checkout/address/create/$', checkout_address_create_view, name='checkout_address_view'),
+    url(r'^checkout/address/create/$', checkout_address_create_view, name='checkout_address_create'),
+    url(r'^checkout/address/reuse/$', checkout_address_reuse_view, name='checkout_address_reuse'),
     url(r'^admin/', admin.site.urls),
 ]
 
