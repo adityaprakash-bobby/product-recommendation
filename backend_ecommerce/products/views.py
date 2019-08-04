2636271ac93f6d3b47a8e8d4e0ef9cec8d59d53f
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-08-04 09:28:36 +0530

    finalize cart update

--- backend_ecommerce/products/views.py
+++ backend_ecommerce/products/views.py
@@ -31,6 +31,12 @@ class ProductListView(ListView):
     def get_queryset(self, *args, **kwargs):
         request = self.request
         return Product.objects.all()
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
 
 
 def product_list_view(request):
