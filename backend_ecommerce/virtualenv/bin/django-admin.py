58148091bff64fb5efb402b74d98c799efe9bfcf
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-08-18 07:31:54 +0530

    removed virtualenv files

--- backend_ecommerce/virtualenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/aditya/Documents/web_dell/backend_ecommerce/virtualenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
